5c5334a73f6d612cbb272a36cb49e3eafe19e9ae
Tom <senteswins@gmail.com>
2021-02-03 16:28:32 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+   
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
+    this.http.post('https://ng-guide-78480-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
+      console.log(responseData);
+    });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    return this.http.get('https://ng-guide-78480-default-rtdb.firebaseio.com/posts.json').subscribe(posts => {
+    console.log(posts);
+    });
+  }
+}
